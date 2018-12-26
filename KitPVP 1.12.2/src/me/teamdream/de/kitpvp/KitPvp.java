1945f4ec04a784c1cda65526b83e99a1144aef92
ArianoMiguel <ArianoMiguel@ArianoMiguel.Speedport_W_724V_01011603_06_002>
2018-12-26 21:14:52 +0100

    Grundgerüst erweitert von: Ariano

--- KitPVP 1.12.2/src/me/teamdream/de/kitpvp/KitPvp.java
+++ KitPVP 1.12.2/src/me/teamdream/de/kitpvp/KitPvp.java
@@ -1,20 +1,92 @@
 package me.teamdream.de.kitpvp;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.teamdream.de.kitpvp.event.Listeners;
+import me.teamdream.de.kitpvp.kit.KitManager;
+//@SuppressWarnings({ "deprecation", "unused" })
+@SuppressWarnings("deprecation")
 public class KitPvp extends JavaPlugin {
-
+	
+	
+	/* Main System Variablen */
+	private static KitPvp instance = null;
+	public static KitPvp getInstance() {return instance;}
+	public String home_path = "plugins/TeamDream/";
+	public String config_path = home_path+"/config.yml";
+	public String stats_path = home_path+"/stats/stats.yml";
+	
+	/* Klassen-Instanzen */
+	private KitManager kitManager;
+	
 	@Override
 	public void onEnable() {
-		
-		
+		preInit();
+		init();
+		postInit();
 		super.onEnable();
 	}
 	@Override
 	public void onDisable() {
-		
-		
 		super.onDisable();
 	}
 	
+	/* Vorbereitung */
+	public void preInit() {
+		KitPvp.instance = this;
+	}
+	
+	/* Bearbeitung */
+	public void init() {
+		/*
+		 * Prüft die Verfügbarkeit der Config:
+		 * Wenn nicht vorhanden: Config wird erstellt
+		 * Wenn vorhanden: Wird geladen und Daten werden Ausgelesen */
+		File file = new File(config_path);
+		FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+		if(!file.exists()) {
+			cfg.set("Server.Config.Main.Home-Path", home_path);
+			cfg.set("Server.Config.Main.Config-Path", config_path);
+			cfg.set("Server.Config.Main.Stats-Path", stats_path);
+			
+			try {cfg.save(file);} catch (IOException e) {e.printStackTrace();}
+		}else{
+		}
+	}
+	/* Fertigstellung */
+	public void postInit() {
+		new Listeners(getInstance());
+		this.kitManager = new KitManager(getInstance());
+	}
+	
+	/* Verschiedenes */
+	public static String getTimeInString() {
+		/*
+		 * Gibt die Aktuelle Zeit wieder
+		 * */
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		Calendar date = sdf.getCalendar();
+		Date d = date.getTime();
+		return d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+	}
+	public static String getDateInString() {
+		/*
+		 * Gibt das aktuelle Datum wieder
+		 * */
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		Calendar date = sdf.getCalendar();
+		Date d = date.getTime();
+		int day = d.getDate();
+		int mon = d.getMonth();
+		int year = d.getYear();
+		return day+":"+mon+":"+year;
+	}
 }
---
+++ KitPVP 1.12.2/src/me/teamdream/de/kitpvp/kit/KitManager.java
@@ -0,0 +1,17 @@
+package me.teamdream.de.kitpvp.kit;
+
+import org.bukkit.entity.Player;
+
+import me.teamdream.de.kitpvp.KitPvp;
+
+public class KitManager {
+	
+	public KitManager(KitPvp kitpvp) {
+		
+	}
+	
+	public void saveKit(String kitname, Player creator) {
+		
+	}
+	
+}
